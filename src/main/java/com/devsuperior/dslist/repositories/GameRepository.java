2a416c64cc00f32fa6d75a7b269430c15a38735a
Pedro Fonseca <125426081+pedrohf0001@users.noreply.github.com>
2025-08-21 21:56:02 -0300

    Projections e fim aula 2

--- src/main/java/com/devsuperior/dslist/repositories/GameRepository.java
+++ src/main/java/com/devsuperior/dslist/repositories/GameRepository.java
@@ -1,7 +1,21 @@
 package com.devsuperior.dslist.repositories;
 
 import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.projections.GameMinProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface GameRepository extends JpaRepository<Game, Long> {
+    //precisa usar essa notation para conseguir fazer a consulta. e quanto se usar o nativeQuery ele espera de retorno uma interface
+    @Query(nativeQuery = true, value = """
+		SELECT tb_game.id, tb_game.title, tb_game.game_year AS `year`, tb_game.img_url AS imgUrl,
+		tb_game.short_description AS shortDescription, tb_belonging.position
+		FROM tb_game
+		INNER JOIN tb_belonging ON tb_game.id = tb_belonging.game_id
+		WHERE tb_belonging.list_id = :listId
+		ORDER BY tb_belonging.position
+			""")
+    List<GameMinProjection> searchByList(Long listId);
 }
